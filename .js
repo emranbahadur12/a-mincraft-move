abe90bcce2c150578a93be7e562bf335b898d260
emranbahadur12 <146596555+emranbahadur12@users.noreply.github.com>
2025-05-21 17:14:24 -0500

    movie.js

--- .js
+++ .js
@@ -1,6 +1,1 @@
-<li class="px-3 py-2.5 text-sm text-gray-200 hover:bg-gray-700/60 cursor-pointer transition-all duration-150
-                        hover:pl-4 hover:text-white active:bg-gray-600/60 first:rounded-t-lg last:rounded-b-lg
-                        border-b border-gray-700/50 last:border-b-0 truncate" role="option" aria-selected="true">Embed.su (primary)</li>
-<li class="px-3 py-2.5 text-sm text-gray-200 hover:bg-gray-700/60 cursor-pointer transition-all duration-150
-                        hover:pl-4 hover:text-white active:bg-gray-600/60 first:rounded-t-lg last:rounded-b-lg
-                        border-b border-gray-700/50 last:border-b-0 truncate" role="option" aria-selected="true">Embed.su (primary)</li>
+<iframe src="https://www.2embed.cc/embed/tt3566834" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen></iframe>
